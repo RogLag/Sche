9f41f33b13c8d8b86c811c1c1da187fed2bd7269
RogLag <hugo.rog@icloud.com>
2022-09-17 11:26:53 +0200

    Error uptade

--- sche.py
+++ sche.py
@@ -28,11 +28,46 @@ tree = app_commands.CommandTree(bot)
 
 ##Commands
 
-@tree.command(name='timetable', guild=discord.Object(id=1018837382633627658))
+@tree.command(name='timetable.set', guild=discord.Object(id=1018837382633627658))
+@app_commands.user_has_permissions(administrator=True)
 async def self(interaction: discord.Interaction):
     await interaction.response.send_message('test')
     
-    
+##Error
+
+@tree.error
+async def error_handler(interaction: discord.Interaction, error: Exception):
+    if isinstance(error, app_commands.CommandNotFound):
+        await interaction.response.send_message(f"Command not found: {error.command_name}")
+    elif isinstance(error, app_commands.MissingPermissions):
+        await interaction.response.send_message(f"Missing permissions: {error.missing_permissions}")
+    elif isinstance(error, app_commands.MissingRequiredArgument):
+        await interaction.response.send_message(f"Missing required argument: {error.argument_name}")
+    elif isinstance(error, app_commands.TooManyArguments):
+        await interaction.response.send_message(f"Too many arguments: {error.argument_name}")
+    elif isinstance(error, app_commands.ArgumentConversionFailure):
+        await interaction.response.send_message(f"Argument conversion failure: {error.argument_name}")
+    elif isinstance(error, app_commands.ArgumentParsingFailure):
+        await interaction.response.send_message(f"Argument parsing failure: {error.argument_name}")
+    elif isinstance(error, app_commands.CommandInvokeError):
+        await interaction.response.send_message(f"Command invoke error: {error.original}")
+    elif isinstance(error, app_commands.CommandError):
+        await interaction.response.send_message(f"Command error: {error.original}")
+    elif isinstance(error, app_commands.CommandRegistrationError):
+        await interaction.response.send_message(f"Command registration error: {error.original}")
+    elif isinstance(error, app_commands.CommandTreeError):
+        await interaction.response.send_message(f"Command tree error: {error.original}")
+    elif isinstance(error, app_commands.CommandTreeRegistrationError):
+        await interaction.response.send_message(f"Command tree registration error: {error.original}")
+    elif isinstance(error, app_commands.CommandTreeUnregistrationError):
+        await interaction.response.send_message(f"Command tree unregistration error: {error.original}")
+    elif isinstance(error, app_commands.CommandUnregistrationError):
+        await interaction.response.send_message(f"Command unregistration error: {error.original}")
+    elif isinstance(error, app_commands.BotMissingPermissions):
+        await interaction.response.send_message(f"Bot missing permissions: {error.missing_permissions}")
+    else:
+        await interaction.response.send_message(f"Error: {error}")
+
 ##Run Bot
 
 bot.run("MTAyMDA0NzI1OTE1NDUzNDU2Mg.GbPWMW.JEq7sHkId4KolE7kZnFNU59dOTsWAJyQvDUC1U")
