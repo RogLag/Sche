920e53f8a1a1f59cade45ddf1106650aff9966c6
RogLag <hugo.rog@icloud.com>
2022-09-22 19:18:57 +0200

    Sche v1.0.0

--- sche.py
+++ sche.py
@@ -1,9 +1,10 @@
+from types import coroutine
 import discord
 from discord.ext import commands
 from discord import app_commands
 from discord.app_commands import Choice
 import datetime
-import time
+import asyncio
 import ics_reader
 
 intents = discord.Intents.default()
@@ -37,20 +38,23 @@ async def setup(interaction: discord.Interaction, group: str):
     else:
         ics_reader.getTimetable(dateofday.year, dateofday.month, dateofday.day, f"{group}", "1", "1")
     await interaction.channel.send(file=discord.File('./calendar.png'))
+    print("ok1")
     while True:
         dateofday = datetime.datetime.now()
         """
         verification que le jour est un lundi
         """
-        if dateofday.hour == 00 and dateofday.minute >= 1 and dateofday.minute <= 5:
+        if dateofday.hour == 00 and dateofday.minute >= 5 and dateofday.minute <= 00:
             if dateofday.weekday() == 0:
                 ics_reader.getTimetable(dateofday.year, dateofday.month, dateofday.day, "0", "1", "1")
             else:
                 ics_reader.getTimetable(dateofday.year, dateofday.month, dateofday.day, f"{group}", "1", "1")
-            await interaction.response.send_message(file=discord.File('./calendar.png'))
-            time.sleep(60*60*24)
+            await interaction.channel.send(file=discord.File('./calendar.png'))
+            print(f"Message bien envoyé à {interaction.channel.name} le {dateofday.day}/{dateofday.month}/{dateofday.year} à {dateofday.hour}:{dateofday.minute}:{dateofday.second}")
+            await asyncio.sleep(60*60*24)
         else:
-            time.sleep(60*60*(19-dateofday.hour)+60*(60-dateofday.minute)+60-dateofday.second)
+            print(f"Le premier message sera envoyé dans {60*60*(24-dateofday.hour)+60*(5-dateofday.minute)+00-dateofday.second} secondes.")
+            await asyncio.sleep(60*60*(24-dateofday.hour)+60*(5-dateofday.minute)+00-dateofday.second)
 
 @bot.tree.command(name="timetable", description="Send the timetable of the day.")
 @app_commands.choices(month = [
@@ -76,19 +80,22 @@ async def timetable(interaction: discord.Interaction, day: str, month: str, year
         if int(day) > 28:
             await interaction.response.send_message("La date entrée n'est pas valide.", ephemeral=True)
             return
-    elif int(month) == 4 or int(month) == 6 or int(month) == 9 or int(month) == 11:
+    if int(month) == 4 or int(month) == 6 or int(month) == 9 or int(month) == 11:
         if int(day) > 30:
             await interaction.response.send_message("La date entrée n'est pas valide.", ephemeral=True)
             return
-    elif int(month) == 1 or int(month) == 3 or int(month) == 5 or int(month) == 7 or int(month) == 8 or int(month) == 10 or int(month) == 12:
+    print("ok3")
+    if int(month) == 1 or int(month) == 3 or int(month) == 5 or int(month) == 7 or int(month) == 8 or int(month) == 10 or int(month) == 12:
         if int(day) > 31:
             await interaction.response.send_message("La date entrée n'est pas valide.", ephemeral=True)
             return
-    else:
-        try:
-            ics_reader.getTimetable(year, month, day, group, englishgroup, sigroup)
-            await interaction.response.send_message(file=discord.File('./calendar.png'), ephemeral=True)
-        except ValueError:
-            await interaction.response.send_message("Wrong date", ephemeral=True)
-
-bot.run('MTAyMDA0NzI1OTE1NDUzNDU2Mg.GbXGjC.3BYj0Bji-p0XWVoEEOxCi2WaqCCUHdDZcVQPEA')
+    try:
+        ics_reader.getTimetable(int(year), int(month), int(day), group, englishgroup, sigroup)
+        await interaction.response.send_message(file=discord.File('./calendar.png'), ephemeral=True)
+    except ValueError:
+        await interaction.response.send_message("Wrong date", ephemeral=True)
+    print(f"Message envoyée dans le channel {interaction.channel.name} à l'utilisateur {interaction.user.name} sur le serveur {interaction.guild.name} le {datetime.datetime.now()}")
+    
+    
+    
+bot.run('MTAyMDA0NzI1OTE1NDUzNDU2Mg.GT06sA.sKPN-GX59xa5c4AX3ocKtPSJsSDTB18LgHHbDg')
