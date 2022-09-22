40459af8f6c3ed31e6edb1f96915c6fbb3bfcb37
Hugo.M <hugo.rog@icloud.com>
2022-09-22 08:19:27 +0200

    Sche v1.0.0

--- sche.py
+++ sche.py
@@ -1,8 +1,10 @@
 import discord
-import asyncio
-import os
 from discord.ext import commands
+from discord import app_commands
+from discord.app_commands import Choice
 import datetime
+import time
+import ics_reader
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -18,14 +20,46 @@ async def on_ready():
     embed=discord.Embed(title=f"{bot.user.name} is ready !", description=f"Up date: {dt_string},\n\nVersion: {version},\n{bot.user.name} by Rog#8698.", color=0x33DAFF)
     await channel_connected.send(embed=embed)
     print(f"{bot.user.name} is ready.")  
+    synced = await bot.tree.sync()
+    print(f"Synced {synced} commands.")
 
-async def load_cogs():
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
-            bot.load_extension(f'cogs.{filename[:-3]}')
+@bot.tree.command(name="setup", description="Setup the bot for timetable of the day, every day.")
+@app_commands.checks.has_permissions(manage_guild=True)
+async def setup(interaction: discord.Interaction):
+    dateofday = datetime.datetime.now()
+    await interaction.response.send_message("Setup the bot for timetable of the day, every day.", ephemeral=True)
+    await interaction.response.send_message(file=discord.File('./calendar.png'), ephemeral=True)
+    while True:
+        dateofday = datetime.datetime.now()
+        if dateofday.hour == 19 and dateofday.minute >= 0 and dateofday.minute <= 5:
+            await interaction.response.send_message(file=discord.File('./calendar.png'), ephemeral=True)
+            time.sleep(60*60*24)
+        else:
+            time.sleep(60*60*(19-dateofday.hour)+60*(60-dateofday.minute)+60-dateofday.second)
 
-async def main():
-    await load_cogs()
-    await bot.start("MTAyMDA0NzI1OTE1NDUzNDU2Mg.GPGJNa.yeqbFGCNogTDEYM-X2bOdCj7VStCLxcjeH3F4Q")
+@bot.tree.command(name="timetable", description="Send the timetable of the day.")
+@app_commands.choices(month = [
+        Choice(name='Janvier', value='1'),
+        Choice(name='Fevrier', value='2'),
+        Choice(name='Mars', value='3'),
+        Choice(name='Avril', value='4'),
+        Choice(name='Mai', value='5'),
+        Choice(name='Juin', value='6'),
+        Choice(name='Juillet', value='7'),
+        Choice(name='Août', value='8'),
+        Choice(name='Septembre', value='9'),
+        Choice(name='Octobre', value='10'),
+        Choice(name='Novembre', value='11'),
+        Choice(name='Décembre', value='12'),
+    ])
+async def timetable(self, interaction: discord.Interaction, day: str, month: str, year: str, englishgroup: str, sigroup: str):
+    if int(day) < 1 or int(day) > 31:
+        await interaction.response.send_message("The day is not valid.", ephemeral=True)
+    else:
+        try:
+            await ics_reader.getTimetable(year, month, day, englishgroup, sigroup)
+            await interaction.response.send_message(file=discord.File('./calendar.png'), ephemeral=True)
+        except ValueError:
+            await interaction.response.send_message("Wrong date", ephemeral=True)
 
-asyncio.run(main())
+bot.run('token')
