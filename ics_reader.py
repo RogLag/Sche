c6ffdbb0a3079fbe2515569aa699f0353a19caff
Proxyfil <piloupilou200@gmail.com>
2023-01-06 22:31:33 +0100

    feat : ics_reader up to date for S2

--- ics_reader.py
+++ ics_reader.py
@@ -17,32 +17,50 @@ def get_wrapped_text(text: str, font: ImageFont.ImageFont, line_length: int):
         return '\n'.join(lines)
 
 def updateTimetable():
-    calendarFile = open('./ADECalA.ics', 'w', encoding='utf-8', newline='\n')
+    calendarFile = open('./ADECal5A.ics', 'w', encoding='utf-8', newline='\n')
 
-    res = requests.get('http://ade.univ-tours.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?data=af235c48b98dffe83858948ea9f61e62b4656713ae4720315366f1bfdaa16957a56b9d48cf60d89aad5fb9b261bdf14bc23fc73e40a62f8cea315d7ede6c0137,1')
+    res = requests.get('http://ade.univ-tours.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?data=3b46878b11fa7c3d580ecb5f6648a9c295c381511cc76ec8bdffe48ddf108006a56b9d48cf60d89aad5fb9b261bdf14bc23fc73e40a62f8cea315d7ede6c0137,1')
     for line in res.text:
         calendarFile.writelines(line)
     calendarFile.close()
 
-    calendarFile = open('./ADECalB.ics', 'w', encoding='utf-8', newline='\n')
+    calendarFile = open('./ADECal5B.ics', 'w', encoding='utf-8', newline='\n')
 
-    res = requests.get('http://ade.univ-tours.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?data=d10d2eee7adfcc1a372dc75a5b67ef22c7b1b8fd4893743159bad83977cb3718a56b9d48cf60d89aad5fb9b261bdf14bc23fc73e40a62f8cea315d7ede6c0137,1')
+    res = requests.get('http://ade.univ-tours.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?data=0244767deedd65be3ee51f7470b1cb71c11996846e88763bd0db20b9ce4a14ada56b9d48cf60d89aad5fb9b261bdf14bc23fc73e40a62f8cea315d7ede6c0137,1')
+    for line in res.text:
+        calendarFile.writelines(line)
+    calendarFile.close()
+    
+    calendarFile = open('./ADECal6A.ics', 'w', encoding='utf-8', newline='\n')
+
+    res = requests.get('http://ade.univ-tours.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?data=5895f33e2f963a3010802e36bbb12558afcee9e49fc2e84b5e5c68df6d2263e5a56b9d48cf60d89aad5fb9b261bdf14bc23fc73e40a62f8cea315d7ede6c0137,1')
+    for line in res.text:
+        calendarFile.writelines(line)
+    calendarFile.close()
+
+    calendarFile = open('./ADECal6B.ics', 'w', encoding='utf-8', newline='\n')
+
+    res = requests.get('http://ade.univ-tours.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?data=503b6f75818e7019255a4e96b4e2e65b95c381511cc76ec8bdffe48ddf108006a56b9d48cf60d89aad5fb9b261bdf14bc23fc73e40a62f8cea315d7ede6c0137,1')
     for line in res.text:
         calendarFile.writelines(line)
     calendarFile.close()
 
 def getTimetable(year, month, day, classGroup, englishGroup=1, siGroup=1):
     hour_delta = -7
-    cal = Calendar.from_ical(open('ADECalA.ics', 'rb').read())
+    cal = Calendar.from_ical(open('ADECal5A.ics', 'rb').read())
     today = datetime.datetime(year, month, day)
     image = Image.new(mode='RGBA',size=(700, 1660),color=(22,22,22,255))
     draw = ImageDraw.Draw(image)
     fnt = ImageFont.truetype("./ayar.ttf", 30, encoding="unic")
 
-    if(classGroup == "A"):
-        cal = Calendar.from_ical(open('ADECalA.ics', 'rb').read())
-    elif(classGroup == "B"):
-        cal = Calendar.from_ical(open('ADECalB.ics', 'rb').read())
+    if(classGroup == "5A"):
+        cal = Calendar.from_ical(open('ADECal5A.ics', 'rb').read())
+    elif(classGroup == "5B"):
+        cal = Calendar.from_ical(open('ADECal5B.ics', 'rb').read())
+    elif(classGroup == "6A"):
+        cal = Calendar.from_ical(open('ADECal6A.ics', 'rb').read())
+    elif(classGroup == "6B"):
+        cal = Calendar.from_ical(open('ADECal6B.ics', 'rb').read())
 
     for component in cal.walk():
         if component.name == "VEVENT":
@@ -55,11 +73,8 @@ def getTimetable(year, month, day, classGroup, englishGroup=1, siGroup=1):
                     color = (100,100,100,255)
                     if('Anglais' in component.get('summary')):
                         color = (100,100,255,255)
-                    elif('Sc_Ingé'):
+                    elif('Projets'):
                         color = (255,100,100,255)
-
-                    if('A'+str(englishGroup) not in component.get('summary') and 'G'+str(siGroup) not in component.get('summary')):
-                        continue
 
 
                     draw.rectangle((50, 50+138*(component.get('dtstart').dt.timetuple().tm_hour+hour_delta)+35*(component.get('dtstart').dt.timetuple().tm_min//15), 650, 50+138*(component.get('dtend').dt.timetuple().tm_hour+hour_delta)+35*(component.get('dtend').dt.timetuple().tm_min//15)), fill=color)
@@ -81,16 +96,12 @@ def getTimetable(year, month, day, classGroup, englishGroup=1, siGroup=1):
                 else:
                     
                     color = (100,100,100,255)
-                    if('Raisonnement' in component.get('summary')):
-                        color = (50,50,255,255)
-                    elif('Calculus' in component.get('summary')):
-                        color = (0,200,200,255)
-                    elif('Electrostatique' in component.get('summary')):
-                        color = (255,128,128,255)
-                    elif('Mécanique' in component.get('summary')):
-                        color = (255,0,128,255)
-                    elif('Informatique' in component.get('summary')):
-                        color = (0,128,64,255)
+                    if('Bases de données' in component.get('summary') or 'BD' in component.get('summary')):
+                        color = (128,128,0,255)
+                    elif('Algèbre' in component.get('summary')):
+                        color = (0,128,128,255)
+                    elif('Analyse' in component.get('summary')):
+                        color = (64,128,64,255)
                     
 
                     draw.rectangle((50, 50+138*(component.get('dtstart').dt.timetuple().tm_hour+hour_delta)+35*(component.get('dtstart').dt.timetuple().tm_min//15), 650, 50+138*(component.get('dtend').dt.timetuple().tm_hour+hour_delta)+35*(component.get('dtend').dt.timetuple().tm_min//15)), fill=color)
